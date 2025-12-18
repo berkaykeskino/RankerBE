b3b72196279bc6b13f2b776daa72ca6c113aa5f5
berkaykeskin <keskinberkay30@gmail.com>
2025-12-18 23:57:18 +0300

    I added get ranking type by id. RankingsPage first fetches rankings and uses this new service to get ranking type base after that. http://localhost:8080/ranking-type/{rankingTypeId}

--- src/main/java/com/berkay/ranker/rankEvent/service/implementation/RankingTypeServiceImpl.java
+++ src/main/java/com/berkay/ranker/rankEvent/service/implementation/RankingTypeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.berkay.ranker.rankEvent.service.implementation;
 
+import com.berkay.ranker.common.exceptionHandling.customExceptions.ResourceNotFoundException;
 import com.berkay.ranker.rankEvent.data.dto.RankingTypeDTO;
 import com.berkay.ranker.rankEvent.data.entity.RankingType;
 import com.berkay.ranker.rankEvent.data.mapper.RankingTypeMapper;
@@ -20,5 +21,12 @@ public class RankingTypeServiceImpl implements RankingTypeService {
         RankingType entity = mapper.toRankingType(request);
         RankingType saved = repository.save(entity);
         return mapper.toRankingTypeDTO(saved);
+    }
+
+    @Override
+    public RankingTypeDTO getRankingType(Long rankingTypeId){
+        RankingType rankingType = repository.findById(rankingTypeId)
+                .orElseThrow(() -> new ResourceNotFoundException("ranking-type.not.found", rankingTypeId));
+        return mapper.toRankingTypeDTO(rankingType);
     }
 }
